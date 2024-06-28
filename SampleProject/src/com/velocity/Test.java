9f9757d91ef04e145c3998d4c59dd94edf3fc43c
Prasad Mule <prasadmule870@gmail.com>
2024-06-28 23:29:27 +0530

    ArrayList code added

---
+++ SampleProject/src/com/velocity/Test.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add("Prasad");
+		al.add("Mahesh");
+		al.add(10);
+		al.add(20);
+		 System.out.println(al);
+	}
+
+}
